ea63bbd7ded6923ec6d6e98adfc236f7fda42576
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-28 20:30:56 +0500

    day 46 challenge

---
+++ index.ts
@@ -0,0 +1,19 @@
+                // 🚀 Day 46 Challenge: Start Coding! 🚀 //
+
+//Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+for (let i = 1; i <= 6; i++) {
+    console.log(`Iteration ${i}, i value:, i`);//Logs the current iteration number and the value of i
+}
+  
+//Question 137: Implement a try-catch block to handle potential errors in a block of code.
+try  {throw new Error("Something went wrong");
+    } catch (error) {          // Intentionally cause an error
+console.log(error.message);   // Logs the error message
+}
+  
+//Question 138: Describe how to use breakpoints in browser developer tools to debug JavaScript code.
+
+console.log("Before breakpoint");  // Sample code snippet for setting a breakpoint
+console.log("This line has a breakpoint set on it in the browser's developer tools"); 
+console.log("After breakpoint");
+
